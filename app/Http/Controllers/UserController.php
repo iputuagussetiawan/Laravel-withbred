9d42fef5fc8eaf459f0bdf43bd4b9039272931a3
iputuagussetiawan <agussetiawaniputu@gmail.com>
2023-02-11 09:08:29 +0800

    39. Removing a Follow

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Follow;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Validation\Rule;
@@ -52,9 +53,13 @@ class UserController extends Controller
 
     public function profile(User $user)
     {
-        $thePosts = $user->posts()->latest()->get();
-        $thePostsCount = $user->posts()->count();
-        return view('profile-posts', ['avatar' => $user->avatar,  'username' => $user->username, 'posts' => $thePosts, 'postCount' => $thePostsCount]);
+        $currentlyFollowing = 0;
+
+        if (auth()->check()) {
+            $currentlyFollowing = Follow::where([['user_id', '=', auth()->user()->id], ['followeduser', '=', $user->id]])->count();
+        }
+
+        return view('profile-posts', ['currentlyFollowing' => $currentlyFollowing, 'avatar' => $user->avatar, 'username' => $user->username, 'posts' => $user->posts()->latest()->get(), 'postCount' => $user->posts()->count()]);
     }
 
     public function showAvatarForm()

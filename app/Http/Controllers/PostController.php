f0baa1b6a4cdc04266d146184e62dfb8cdf957ce
iputuagussetiawan <agussetiawaniputu@gmail.com>
2023-02-11 14:54:49 +0800

    47. Search (Back-End)

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -69,4 +69,12 @@ class PostController extends Controller
 
         return back()->with('success', 'Post successfully updated.');
     }
+
+    public function search($term)
+    {
+        $posts = Post::search($term)->get();
+        $posts->load('user:id,username,avatar');
+        return $posts;
+        //return Post::where('title', 'LIKE', '%' . $term . '%')->orWhere('body', 'LIKE', '%' . $term . '%')->with('user:id,username,avatar')->get();
+    }
 }

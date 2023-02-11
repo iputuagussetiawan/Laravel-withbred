da55cf3f974c6a43a6b193ac905ec0ee11711747
iputuagussetiawan <agussetiawaniputu@gmail.com>
2023-02-11 11:24:25 +0800

    40-41. Profile Pages

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,41 +2,81 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Follow;
 use App\Models\User;
+use App\Models\Follow;
 use Illuminate\Http\Request;
 use Illuminate\Validation\Rule;
+use Illuminate\Support\Facades\View;
 use Intervention\Image\Facades\Image;
 use Illuminate\Support\Facades\Storage;
 
 class UserController extends Controller
 {
-    public function register(Request $request)
+    public function storeAvatar(Request $request)
     {
+        $request->validate([
+            'avatar' => 'required|image|max:3000'
+        ]);
 
-        $incomingFields = $request->validate([
-            'username' => ['required', 'min:3', 'max:20', Rule::unique('users', 'username')],
-            'email' => ['required', 'email', Rule::unique('users', 'email')],
-            'password' => ['required', 'min:8', 'confirmed'],
-        ]);
-        $incomingFields['password'] = bcrypt($incomingFields['password']);
-        $user = User::create($incomingFields);
-        auth()->login($user);
-        return redirect('/')->with('success', 'Thank You For Creating Account');
-    }
-    public function login(Request $request)
-    {
-        $incomingFields = $request->validate([
-            'loginusername' => ['required'],
-            'loginpassword' => ['required'],
-        ]);
-        if (auth()->attempt(['username' => $incomingFields['loginusername'], 'password' => $incomingFields['loginpassword']])) {
-            $request->session()->regenerate();
-            return redirect('/')->with('success', 'You Have Succesfully Login');
-        } else {
-            return redirect('/')->with('failure', 'Invalid Login');
+        $user = auth()->user();
+
+        $filename = $user->id . '-' . uniqid() . '.jpg';
+
+        $imgData = Image::make($request->file('avatar'))->fit(120)->encode('jpg');
+        Storage::put('public/avatars/' . $filename, $imgData);
+
+        $oldAvatar = $user->avatar;
+
+        $user->avatar = $filename;
+        $user->save();
+
+        if ($oldAvatar != "/fallback-avatar.jpg") {
+            Storage::delete(str_replace("/storage/", "public/", $oldAvatar));
         }
+
+        return back()->with('success', 'Congrats on the new avatar.');
     }
+
+    public function showAvatarForm()
+    {
+        return view('avatar-form');
+    }
+
+    private function getSharedData($user)
+    {
+        $currentlyFollowing = 0;
+
+        if (auth()->check()) {
+            $currentlyFollowing = Follow::where([['user_id', '=', auth()->user()->id], ['followeduser', '=', $user->id]])->count();
+        }
+
+        View::share('sharedData', ['currentlyFollowing' => $currentlyFollowing, 'avatar' => $user->avatar, 'username' => $user->username, 'postCount' => $user->posts()->count()]);
+    }
+
+    public function profile(User $user)
+    {
+        $this->getSharedData($user);
+        return view('profile-posts', ['posts' => $user->posts()->latest()->get()]);
+    }
+
+    public function profileFollowers(User $user)
+    {
+        $this->getSharedData($user);
+        return view('profile-followers', ['posts' => $user->posts()->latest()->get()]);
+    }
+
+    public function profileFollowing(User $user)
+    {
+        $this->getSharedData($user);
+        return view('profile-following', ['posts' => $user->posts()->latest()->get()]);
+    }
+
+    public function logout()
+    {
+        auth()->logout();
+        return redirect('/')->with('success', 'You are now logged out.');
+    }
+
     public function showCorrectHomepage()
     {
         if (auth()->check()) {
@@ -45,43 +85,34 @@ class UserController extends Controller
             return view('homepage');
         }
     }
-    public function logout()
+
+    public function login(Request $request)
     {
-        auth()->logout();
-        return redirect('/')->with('success', 'You are now logged out.');
-    }
-
-    public function profile(User $user)
-    {
-        $currentlyFollowing = 0;
-
-        if (auth()->check()) {
-            $currentlyFollowing = Follow::where([['user_id', '=', auth()->user()->id], ['followeduser', '=', $user->id]])->count();
-        }
-
-        return view('profile-posts', ['currentlyFollowing' => $currentlyFollowing, 'avatar' => $user->avatar, 'username' => $user->username, 'posts' => $user->posts()->latest()->get(), 'postCount' => $user->posts()->count()]);
-    }
-
-    public function showAvatarForm()
-    {
-        return view('avatar-form');
-    }
-
-    public function storeAvatar(Request $request)
-    {
-        $request->validate([
-            'avatar' => 'required|image|max:3000'
+        $incomingFields = $request->validate([
+            'loginusername' => 'required',
+            'loginpassword' => 'required'
         ]);
-        $user = auth()->user();
-        $filename = $user->id . '-' . uniqid() . '.jpg';
-        $imgData = Image::make($request->file('avatar'))->fit(120)->encode('jpg');
-        Storage::put('public/avatars/' . $filename, $imgData);
-        $oldAvatar = $user->avatar;
-        $user->avatar = $filename;
-        $user->save();
-        if ($oldAvatar != "/fallback-avatar.jpg") {
-            Storage::delete(str_replace("/storage/", "public/", $oldAvatar));
+
+        if (auth()->attempt(['username' => $incomingFields['loginusername'], 'password' => $incomingFields['loginpassword']])) {
+            $request->session()->regenerate();
+            return redirect('/')->with('success', 'You have successfully logged in.');
+        } else {
+            return redirect('/')->with('failure', 'Invalid login.');
         }
-        return back()->with('success', 'Congrats on the new avatar.');
+    }
+
+    public function register(Request $request)
+    {
+        $incomingFields = $request->validate([
+            'username' => ['required', 'min:3', 'max:20', Rule::unique('users', 'username')],
+            'email' => ['required', 'email', Rule::unique('users', 'email')],
+            'password' => ['required', 'min:8', 'confirmed']
+        ]);
+
+        $incomingFields['password'] = bcrypt($incomingFields['password']);
+
+        $user = User::create($incomingFields);
+        auth()->login($user);
+        return redirect('/')->with('success', 'Thank you for creating an account.');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -47,3 +47,5 @@ Route::get('/profile/{user:username}', [UserController::class, 'profile']);
 // Follow related routes
 Route::post('/create-follow/{user:username}', [FollowController::class, 'createFollow'])->middleware('mustBeLoggedIn');
 Route::post('/remove-follow/{user:username}', [FollowController::class, 'removeFollow'])->middleware('mustBeLoggedIn');
+Route::get('/profile/{user:username}/followers', [UserController::class, 'profileFollowers']);
+Route::get('/profile/{user:username}/following', [UserController::class, 'profileFollowing']);
